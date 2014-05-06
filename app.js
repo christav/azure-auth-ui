9ee0feac0d0748206f2838a68f2a274676bc5e4d
Chris Tavares <ctavares@microsoft.com>
2014-05-05 21:13:42 -0700

    Rearranged auth setup into separate file.
    Factored the authentication bits out into a separate file and router,
    so that I can easily keep the passport details separated from the
    core app setup.
    
    Also removed extraneous debug log messages.

--- app.js
+++ app.js
@@ -6,28 +6,8 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var session = require('cookie-session');
 var flash = require('connect-flash');
-var passport = require('passport');
-var LocalStrategy = require('passport-local').Strategy;
-
+var authSetup = require('./lib/auth-setup');
 var routes = require('./routes');
-
-passport.use(new LocalStrategy(
-  function (username, password, done) {
-    if (username === 'chris' && password === 'secret') {
-      return done(null, { username: 'chris' });
-    }
-    return done(null, false, { message: 'Unknown user'});
-  }));
-
-passport.serializeUser(function (user, done) {
-  console.log("Serializing user", user);
-  done(null, user);
-});
-
-passport.deserializeUser(function (user, done) {
-  console.log('passport deserializing user', user);
-  done(null, user);
-});
 
 var app = express();
 
@@ -47,9 +27,9 @@ app.use(express.static(path.join(__dirname, 'bower_components')));
 app.use(session({
     secret: 'new secret'
 }));
+
 app.use(flash());
-app.use(passport.initialize());
-app.use(passport.session());
+app.use(authSetup);
 
 app.use('/', routes.root);
 app.use('/users', routes.users);
--- routes/root.js
+++ routes/root.js
@@ -6,8 +6,6 @@ var util = require('util');
 router.get('/', function(req, res) {
   var n = req.session.views || 0;
   req.session.views = n + 1;
-  console.log('index page, session =', util.inspect(req.session));
-  console.log('index page, user =', req.user);
   res.render('index', { title: 'Express in the hizzouse', n: n, user: req.user });
 });
 
