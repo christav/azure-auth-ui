2bac517c284f14e58d86f721e129bf5c79d9c012
Chris Tavares <ctavares@microsoft.com>
2014-05-06 00:02:55 -0700

    Making a call against github after authorization

--- routes/root.js
+++ routes/root.js
@@ -1,13 +1,36 @@
 var express = require('express');
 var router = express.Router();
 var util = require('util');
+var GitHubApi = require('github');
 
 /* GET home page. */
 router.get('/', function(req, res) {
   var n = req.session.views || 0;
   req.session.views = n + 1;
-  console.log('user =', util.inspect(req.user));
-  res.render('index', { title: 'Express in the hizzouse', n: n, user: req.user });
+
+  var organizations = [];
+  if (req.user) {
+    var github = new GitHubApi({
+      version: "3.0.0"
+    });
+    github.authenticate({
+      type: 'oauth',
+      token: req.user.accessToken
+    });
+
+    github.user.getOrgs({}, function (err, orgs) {
+      if (err) {
+        organizations.push('Error: ' + err.message);
+      } else {
+        orgs.forEach(function (orgData) {
+          organizations.push(orgData.login);
+        });
+      }
+      res.render('index', { title: 'Your github orgs', n: n, user: req.user, orgs: organizations});
+    });
+  } else {
+    res.render('index', { title: 'Express in the hizzouse', n: n });
+  }
 });
 
 module.exports = router;
