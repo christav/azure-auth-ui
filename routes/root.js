f7a504c0633401a1be2743ebd0f745a93f771efe
Chris Tavares <ctavares@microsoft.com>
2014-05-05 22:44:00 -0700

    Trying to get more info from github
    Setting scope in oauth token. Authentication has started failing.

--- routes/root.js
+++ routes/root.js
@@ -6,6 +6,7 @@ var util = require('util');
 router.get('/', function(req, res) {
   var n = req.session.views || 0;
   req.session.views = n + 1;
+  console.log('user =', util.inspect(req.user));
   res.render('index', { title: 'Express in the hizzouse', n: n, user: req.user });
 });
 
