3ec270777a2ff43e768cd61de95f327bb2a3c602
Chris Tavares <ctavares@microsoft.com>
2014-05-12 00:40:31 -0700

    Adduser form selecting organization
    Added filling of select, plus a little javascript to display the
    description of the organization you've picked.

---
+++ public/javascripts/adduser.js
@@ -0,0 +1,22 @@
+//
+// Some code to make the add user form more interactive
+//
+
+  var organizationSelect;
+  var organizationDescription;
+
+  function onChange() {
+    var key = organizationSelect.selectedOptions[0].value;
+    organizationDescription.textContent = orgs[key].description;
+  }
+
+  function onReady() {
+    organizationSelect = document.getElementById('organizationSelect');
+    organizationDescription = document.getElementById('orgDescription');
+
+    organizationSelect.addEventListener('change', onChange);
+    onChange();
+  }
+
+  document.addEventListener('DOMContentLoaded', onReady);
+
