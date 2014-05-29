bb95a4e4e624f6a47404fc35880b786ea1d42256
Chris Tavares <ctavares@microsoft.com>
2014-05-28 23:02:34 -0700

    Naming upates for consistency

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var passport = require('passport');
-var forkController = require('../controllers/createForkController');
+var controller = require('../controllers/createForkController');
 var render = require('../lib/render');
 var router = express.Router();
 
-router.post('/createfork', forkController.createFork);
+router.post('/createfork', controller.createFork);
 
-router.get('/waitforfork', forkController.pollForFork, render.template('waitforfork'));
+router.get('/waitforfork', controller.pollForFork, render.template('waitforfork'));
 
 module.exports = router;
