04b36ece59912c092220dbfff61a4ee29a3bdb68
Chris Tavares <ctavares@microsoft.com>
2014-05-30 22:45:39 -0700

    Figured out implicit  binding in knockout

--- public/javascripts/adduser.js
+++ public/javascripts/adduser.js
@@ -15,10 +15,7 @@
       }
     });
 
-    self.users = ko.observableArray(userData.map(function (user) {
-      user.parent = self;
-      return user;
-    }));
+    self.users = ko.observableArray(userData);
 
     self.githubUserToAdd = ko.observable();
     self.microsoftAliasToAdd = ko.observable();
@@ -30,8 +27,7 @@
     self.addUser = function () {
       self.users.push({
         githubUser: self.githubUserToAdd(),
-        microsoftAlias: self.microsoftAliasToAdd(),
-        parent: self
+        microsoftAlias: self.microsoftAliasToAdd()
       });
       self.githubUserToAdd('');
       self.microsoftAliasToAdd('');
@@ -47,46 +43,3 @@
     ko.applyBindings(new AddUserViewModel(orgs, users));
   });
 }());
-
-// $(function () {
-
-//   // Handle organization selection - update description
-//   $('#organizationSelect').change(function () {
-//     var key = $('#organizationSelect').val();
-//     $('#orgDescription').text(orgs[key].description);
-//   }).change();
-
-//   // Handle enable/disable of the add user button
-//   function onUserChanged() {
-//     var disabled = ($('#githubUser').val() === "") || ($('#microsoftAlias').val() === "");
-//     if (disabled) {
-//       $('#addUserButton').attr('disabled', 'disabled');
-//     } else {
-//       $('#addUserButton').removeAttr('disabled');
-//     }
-//   }
-//   onUserChanged();
-
-//   $('#githubUser').on('change keypress paste input', onUserChanged);
-//   $('#microsoftAlias').on('change keypress paste input', onUserChanged);
-
-//   // Handle pressing the add-user button
-//   $('#addUserButton').click(function () {
-//     var rowNum = $('.users-to-add-group .row').length - 1;
-//     var newRow = $('#userLineTemplate').clone();
-//     newRow.attr('id', 'userRow' + rowNum);
-//     $('input:first', newRow).val($('#githubUser').val()).attr('readonly', 'readonly');
-//     $('input:last', newRow).val($('#microsoftAlias').val()).attr('readonly', 'readonly');
-
-//     newRow.insertBefore($('.users-to-add-group .row:last'));
-
-//     $('#githubUser').val('');
-//     $('#microsoftAlias').val('');
-//     $('#githubUser').focus();
-//   });
-
-//   // handling pressing the remove buttons
-//   $('.users-to-add-group').on('click', '.remove-user-btn', function () {
-//     var row = $(this).parents('.row').remove();
-//   });
-// });
