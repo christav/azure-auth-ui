6578093afa3726cbc85aa111859b799c01464e75
Chris Tavares <ctavares@microsoft.com>
2014-06-01 01:39:20 -0700

    Implementing model as object, not functions.
    It just makes more sense as a model object with methods.

--- models/addUserModel.js
+++ models/addUserModel.js
@@ -3,51 +3,115 @@
 //
 
 var _ = require('lodash');
+var Q = require('q');
 
 //
-// Create a view model for a get request
+// Object representing a model for data received
+// from a post to the add user page
 //
-function emptyReadModel(githubClient) {
-	return githubClient.getOrgFile()
-		.then(function (orgFile)) {
-			var orgs = orgFile.getOrganizations();
 
-			return {
-				orgs: orgs,
-				selectedOrg: orgs[0].key,
-				users: [],
-				errors: []
-			};
-		}
+function AddUserPostModel(githubClient, postBody) {
+  this.github = githubClient;
+  // Kick of fetch of org file and save promise
+  // so we can attach to it later.
+  this.orgFile = this.github.getOrgFile();
+  this.body = postBody;
+  this.users = [];
+  this.errors = [];
 }
 
-function fromPost(githubClient, postBody, resultHandlers) {
+_.extend(AddUserPostModel.prototype, {
+  //
+  // Create read model that doesn't contain any users.
+  // Returns promise for the read model.
+  //
+  emptyReadModel: function () {
+    return this.orgFile
+      .then(function (orgFile) {
+        var orgs = orgFile.getOrganizations();
+        return {
+          orgs: orgs,
+          selectedOrg: orgs[0].key,
+          users: this.users,
+          errors: this.errors
+        };
+      });
+  },
 
-}
+  //
+  // Check if post is valid - returns promise for
+  // bool - true ok, false not, with this.errors set
+  // to array of error messages.
+  //
+  isValidPost: function () {
+    var self = this;
 
-function validatePostData(orgFile, postBody) {
-	var errors = [];
-	if (!postBody.orgToUpdate) {
-		errors.push('No organization given');
-	} else if (!_.has(orgFile.getOrgMap(), postBody.orgToUpdate)) {
-		errors.push('Organization given does not exist');
-	}
+    return self.orgFile
+      .then(function (orgFile) {
+        if (!self.postBody.orgToUpdate) {
+          self.errors.push('No organization given');
+        } else if (!_.has(orgFile.getOrgMap(), postBody.orgToUpdate)) {
+          self.errors.push('Organization given does not exist');
+        }
 
-	var githubUsers = postBody.githubUser;
-	var microsoftAliases = postBody.microsoftAlias;
+        var githubUsers = self.postBody.githubUser;
+        var microsoftAliases = self.postBody.microsoftAlias;
 
-	if (!githubUser || !microsoftAlias) {
-		errors.push('Missing user data');
-	}
+        if (!githubUsers || !microsoftAliass) {
+          errors.push('Missing user data');
+        }
 
-	if (githubUsers && microsoftAlias && (githubUsers.length !== microsoftAliases.length)) {
-		errors.push('User names and aliases do not match up!');
-	}
+        if (githubUsers && microsoftAlias && (githubUsers.length !== microsoftAliases.length)) {
+          self.errors.push('User names and aliases do not match up!');
+        }
 
-	return errors;
-}
+        return errors.length === 0;
+    });
+  },
 
-_.extend(exports, {
-	emptyReadModel: emptyReadModel,
-	fromPost: fromPost
+  // Take the post body, set the this.users field to user objects.
+  function usersFromPostBody: function () {
+    // Use _.flatten to ensure that the list of values are arrays even if only one item
+    var githubUsers = _.flatten(this.postBody.githubUser);
+    var msAliases = _.flatten(this.postBody.microsoftAlias);
+
+    // Turn pair of lists into list of pairs
+    this.users = _.zip(githubUsers, msAliases)
+      .map(function (pair) {
+        return {
+          githubUser: pair[0],
+          microsoftAlias: pair[1],
+          errorMessage: ''
+        };
+      });
+  },
+
+  //
+  // Are all the users listed valid?
+  // Returns promise for bool, sets errors if failure
+  //
+  areValidUsers: function () {
+    usersFromPostBody();
+
+    var self = this;
+    return self.orgFile
+      .then(function (orgFile) {
+        return Q.all(
+          self.users.map(function (user) {
+            return self.github.userExists(user.githubUser)
+              .then(function (exists) {
+                if (!exists) {
+                  user.errorMessage = 'Github user does not exist';
+                  return false;
+                }
+                return true;
+              });
+          }))
+      })
+      .then(function (eachExists) {
+        return _.all(eachExists));
+      });
+  }
 });
+
+module.exports = AddUserPostModel;
